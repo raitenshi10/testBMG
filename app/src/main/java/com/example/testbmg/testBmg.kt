b2901837a1f5ea7bfe4c68b812739ff26260c8b8
ilham aliyudin <ilhampti3@gmail.com>
2021-07-28 10:15:56 +0700

    initial push

---
+++ app/src/main/java/com/example/testbmg/testBmg.kt
@@ -0,0 +1,18 @@
+package com.example.testbmg
+
+fun main() {
+ test(50)
+}
+
+fun test(n: Int) {
+    for(i in 1..n) {
+        when{
+            i % 15 == 0 -> print("Backend FrontEnd")
+            i % 6 == 0 -> print("Backend")
+            i % 5 == 0 -> print("FrontEnd")
+            i % 3 == 0 -> print("Frontend")
+            else -> print("$i, ")
+        }
+    }
+   
+}
